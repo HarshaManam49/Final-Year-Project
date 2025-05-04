98aa148a8bff0748c6c023614efbe2c2ef25440e
HarshaManam49 <98078492+HarshaManam49@users.noreply.github.com>
2025-05-04 15:46:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
